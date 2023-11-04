ea796413b4a700e2cda5219da59afff65f011184
narumi <toucans-cutouts0f@icloud.com>
2023-11-04 15:47:52 +0800

    update

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from poetry.console.application import Application
 
 def main():
     version = Application().poetry.package.pretty_version
+    print("version:", version)
 
     prefix = os.getenv("INPUT_VERSION_PREFIX")
 
     version_tag = f"{prefix}{version}"
+    print("version_tag:", version_tag)
 
     token = os.getenv("INPUT_GITHUB_TOKEN")
     if token is None:
         return
+
     g = Github(token)
     repo = g.get_repo(os.environ["github"]["repository"])
 
@@ -23,6 +26,7 @@ def main():
 
     sha = repo.get_commits()[0].sha
     repo.create_git_ref(f"refs/tags/{version_tag}", sha)
+    print("created tag:", version_tag)
 
 
 if __name__ == "__main__":
