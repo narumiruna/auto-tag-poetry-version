93be83d505747068d80144292cbebe2e7a9de50a
narumi <toucans-cutouts0f@icloud.com>
2023-11-04 21:03:01 +0800

    simplify

--- main.py
+++ main.py
@@ -8,28 +8,18 @@ from poetry.console.application import Application
 @click.argument("token")
 @click.argument("repo")
 def main(prefix, token, repo):
-    try:
-        version = Application().poetry.package.pretty_version
-    except RuntimeError as e:
-        print(e)
-        return
+    version = Application().poetry.package.pretty_version
+    vtag = f"{prefix}{version}"
 
-    version_tag = f"{prefix}{version}"
-
-    if token is None:
-        print("GITHUB_TOKEN not set, skipping tag creation")
-        return
-
-    g = Github(token)
-    repo = g.get_repo(repo)
-
+    repo = Github(token).get_repo(repo)
     for tag in repo.get_tags():
-        if tag.name == version_tag:
+        if tag.name == vtag:
             return
 
-    sha = repo.get_commits()[0].sha
-    repo.create_git_ref(f"refs/tags/{version_tag}", sha)
-    print("created tag:", version_tag)
+    repo.create_git_ref(
+        ref=f"refs/tags/{vtag}",
+        sha=repo.get_commits()[0].sha,
+    )
 
 
 if __name__ == "__main__":
